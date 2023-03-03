4708592f4f102571b740356a0d7e5166bce3e2a2
Alina Negro <alinanegro@mail.ru>
2023-03-03 16:40:00 +0300

    add task 3,4

--- src/index.js
+++ src/index.js
@@ -13,3 +13,20 @@ const a = 2000;
 const songName = `${cityName} ${a}`;
 
 console.log(songName);
+
+//Задание 3
+const messageTime = 128;
+const likesTime = 254;
+const vkTime = 137;
+const youTubeTime = 201;
+
+const procrastinationMinutes = messageTime + likesTime + vkTime + youTubeTime;
+const procrastinationHours = procrastinationMinutes / 60;
+
+console.log (procrastinationHours);
+
+//Задание 4
+const b = 2**5;
+
+console.log(b)
+
