e74d0a5e1105ea7819d71c9830be7f17be1fd2b3
Alina Negro <alinanegro@mail.ru>
2023-03-03 16:38:49 +0300

    add task 1, 2

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Задание 1
+const salaryCleaner = 40000;
+const salaryVocalTeacher = 22000;
+
+const salaryTotal = salaryCleaner + salaryVocalTeacher;
+
+console.log(salaryTotal);
+
+//Задание 2
+const cityName = 'Владивосток';
+const a = 2000;
+
+const songName = `${cityName} ${a}`;
+
+console.log(songName);
