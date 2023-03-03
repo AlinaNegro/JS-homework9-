ad8ffae6fd4bae6444bde1d1b43669da58450c74
Alina Negro <alinanegro@mail.ru>
2023-03-03 16:41:26 +0300

    add task 7

--- src/index.js
+++ src/index.js
@@ -41,3 +41,13 @@ const catGreeting = `${greetings}, ${name}`
 
 console.log(catGreeting);
 
+//Задание 7
+let time;
+time = 34;
+console.log(`'Старт поездки. Осталось минут: '` + time);
+time -= 15;
+console.log(`'Немного сторис в соцсетях. Осталось минут: '` + time);
+time -=10;
+console.log(`'Немного не новостей, но событий. Осталось минут: '` + time);
+time = 0;
+console.log(`'Вы приехали. Добро пожаловать в Москву!'`);
